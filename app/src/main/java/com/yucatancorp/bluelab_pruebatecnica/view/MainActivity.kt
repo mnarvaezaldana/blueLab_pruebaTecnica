e6d3e8d5e1b4a893df31a2e620dbbce8c6df0b53
MarcosArmando <narvaezaldana@gmail.com>
2022-07-24 12:24:07 -0500

    create layout for mainActivity and create space for the fragments

--- app/src/main/java/com/yucatancorp/bluelab_pruebatecnica/view/MainActivity.kt
+++ app/src/main/java/com/yucatancorp/bluelab_pruebatecnica/view/MainActivity.kt
@@ -3,6 +3,7 @@ package com.yucatancorp.bluelab_pruebatecnica.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.navigation.findNavController
 import com.yucatancorp.bluelab_pruebatecnica.R
 import com.yucatancorp.bluelab_pruebatecnica.viewModel.MoviesViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,5 +18,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         moviesViewModel.doNetworkCalls()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_graph)
+        return navController.navigateUp()
     }
 }
